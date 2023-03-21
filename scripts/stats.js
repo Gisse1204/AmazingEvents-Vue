5afaab0d42d8aaf1826d62e8eff4955924d8f97d
Gisse1204 <gise_17@hotmail.com>
2023-03-21 20:58:31 -0300

    uso de un solo reduce

--- scripts/stats.js
+++ scripts/stats.js
@@ -5,32 +5,31 @@ async function printstats1() {
     let response = await fetchResponse.json();
     let arrayEventos = response.events;
 
-    // Ordenar el array por asistencia
+    // Ordeno el array por asistencia
     arrayEventos = arrayEventos.sort((evento1, evento2) => evento1.assistance - evento2.assistance);
 
-    // Encontrar el valor máximo y mínimo de asistencia
-    let maxAssistance = arrayEventos[arrayEventos.length - 1].assistance;
-    let minAssistance = arrayEventos[0].assistance;
+    // Calculo el porcentaje de asistencia de cada evento utilizando la función map()
+    arrayEventos = arrayEventos.map(evento => {
+      return {
+        ...evento,
+        percentAssistance: (evento.assistance / evento.capacity) * 100
+      };
+    });
 
-    // Calcular el porcentaje de asistencia de cada evento
-    for (let i = 0; i < arrayEventos.length; i++) {
-      arrayEventos[i].percentAssistance = (arrayEventos[i].assistance / arrayEventos[i].capacity) * 100;
-    }
-
-    // Ordenar el array por porcentaje de asistencia
+    // Ordeno el array por porcentaje de asistencia
     arrayEventos = arrayEventos.sort((evento1, evento2) => evento1.percentAssistance - evento2.percentAssistance);
 
-    // Encontrar el valor máximo y mínimo de porcentaje de asistencia
+    // Encuentro el valor máximo y mínimo de porcentaje de asistencia
     let maxPercentAssistance = arrayEventos[arrayEventos.length - 1].percentAssistance;
     let minPercentAssistance = arrayEventos[0].percentAssistance;
 
-    // Asignar los valores máximos y mínimos de porcentaje de asistencia a las variables correspondientes
+    // Asigno los valores máximos y mínimos de porcentaje de asistencia a las variables correspondientes
     document.getElementById("highest").innerHTML = arrayEventos[arrayEventos.length - 1].name;
     document.getElementById("highestValue").innerHTML = maxPercentAssistance.toFixed(2) + "%";
     document.getElementById("lowest").innerHTML = arrayEventos[0].name;
     document.getElementById("lowestValue").innerHTML = minPercentAssistance.toFixed(2) + "%";
 
-    // Ordenar el array por capacidad
+    // Ordeno el array por capacidad
     arrayEventos = arrayEventos.sort((evento1, evento2) => evento1.capacity - evento2.capacity);
     document.getElementById("capacidad").innerHTML = arrayEventos[arrayEventos.length - 1].name;
     document.getElementById("capacidadValue").innerHTML = arrayEventos[arrayEventos.length - 1].capacity.toLocaleString();
@@ -47,13 +46,19 @@ async function printstats2() {
     let categorias = [...new Set(arrayEventos.map(evento => evento.category))]; //Obtiene las categorías de eventos únicas mediante el uso de Set y map, y las almacena en una variable llamada categorias
     categorias = categorias.sort(); // Ordenar alfabéticamente
     arrayEventos.forEach(evento => evento.ganancia = evento.estimate * evento.price); //Agrega una nueva propiedad llamada ganancia a cada objeto de evento en arrayEventos multiplicando el número estimado de asistentes por el precio de la entrada
-    let datos2 = categorias.map(category => { //Crea una nueva variable llamada datos2 que es un array de strings, donde cada string representa una fila de la tabla. Cada fila representa una categoría de evento diferente y contiene información como la ganancia total, el porcentaje de asistencia y la capacidad total
-      let eventosFiltro = arrayEventos.filter(evento => evento.category === category);
-      let ganancia = eventosFiltro.reduce((acum, evento) => acum + evento.ganancia, 0);
-      let asistenciat = eventosFiltro.reduce((acum, evento) => acum + evento.estimate, 0);
-      let capitalt = eventosFiltro.reduce((acum, evento) => acum + evento.capacity, 0);
+    let datos2 = categorias.map(category => {
+      let { ganancia, asistencia, capacidad } = arrayEventos.filter(evento => evento.category === category)
+        .reduce((acum, evento) => {
+          return {
+            ganancia: acum.ganancia + evento.ganancia,
+            asistencia: acum.asistencia + evento.estimate,
+            capacidad: acum.capacidad + evento.capacity
+          };
+        }, { ganancia: 0, asistencia: 0, capacidad: 0 });
+    
       let gananciaFormateada = ganancia.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-      let porcentaje = (asistenciat / capitalt * 100).toFixed(2);
+      let porcentaje = (asistencia / capacidad * 100).toFixed(2);
+    
       return `<tr class="d-flex justify-content-center text-center" style="font-weight: bold;">
               <td>${category}</td>
               <td>$${gananciaFormateada}</td>
@@ -73,15 +78,21 @@ async function printstats3() {
     let response = await fetch(urlApi).then(res => res.json());
     let arrayEventos = response.events;
     let categorias = [...new Set(arrayEventos.map(evento => evento.category))];
-    categorias = categorias.sort(); // Ordenar alfabéticamente
+    categorias = categorias.sort();
     arrayEventos.forEach(evento => evento.ganancia = evento.assistance * evento.price);
     let datos3 = categorias.map(category => {
-      let eventosFiltro = arrayEventos.filter(evento => evento.category === category);
-      let ganancia = eventosFiltro.reduce((acum, evento) => acum + evento.ganancia, 0);
-      let asistenciat = eventosFiltro.reduce((acum, evento) => acum + evento.assistance, 0);
-      let capitalt = eventosFiltro.reduce((acum, evento) => acum + evento.capacity, 0);
+      let { ganancia, asistencia, capacidad } = arrayEventos.filter(evento => evento.category === category)
+        .reduce((acum, evento) => {
+          return {
+            ganancia: acum.ganancia + evento.ganancia,
+            asistencia: acum.asistencia + evento.assistance,
+            capacidad: acum.capacidad + evento.capacity
+          };
+        }, { ganancia: 0, asistencia: 0, capacidad: 0 });
+    
       let gananciaFormateada = ganancia.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-      let porcentaje = (asistenciat / capitalt * 100).toFixed(2);
+      let porcentaje = (asistencia / capacidad * 100).toFixed(2);
+    
       return `<tr class="d-flex justify-content-center text-center" style="font-weight: bold;">
               <td>${category}</td>
               <td>$${gananciaFormateada}</td>
